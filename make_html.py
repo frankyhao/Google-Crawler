573a64d46b93046063e1100943d0669ad869ac03
Ubuntu <ubuntu@ip-172-31-39-149.ec2.internal>
2019-06-14 21:30:50 +0000

    updated html

---
+++ make_html.py
@@ -0,0 +1,16 @@
+import os
+
+
+
+with open('index.html', 'w') as f:
+	f.write('<!DOCTYPE html>')
+	f.write('<html>')
+	f.write('<body>')
+	for entry in os.scandir('maps/'):
+		if (entry.is_dir()):
+			f.write('<h1>{}</h1>'.format(entry.name))
+			for mapfile in os.listdir(entry.path):
+				print(os.path.join(entry.path, mapfile))
+				f.write('<a href="{}">{}</a>'.format(os.path.join(entry.path, mapfile), mapfile.split('_')[1][:-5]))
+	f.write('</body>')
+	f.write('</html>')
