be62fdbb74a8d4b66765f9564951d21b2758e4a3
Ubuntu <ubuntu@ip-172-31-39-149.ec2.internal>
2019-06-14 21:33:53 +0000

    added breaks in html

--- make_html.py
+++ make_html.py
@@ -11,6 +11,6 @@ with open('index.html', 'w') as f:
 			f.write('<h1>{}</h1>'.format(entry.name))
 			for mapfile in os.listdir(entry.path):
 				print(os.path.join(entry.path, mapfile))
-				f.write('<a href="{}">{}</a>'.format(os.path.join(entry.path, mapfile), mapfile.split('_')[1][:-5]))
+				f.write('<a href="{}">{}</a><br>'.format(os.path.join(entry.path, mapfile), mapfile.split('_')[1][:-5]))
 	f.write('</body>')
 	f.write('</html>')
