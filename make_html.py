1caf8d004615e345a041aae38c2b0495c877bb12
Ubuntu <ubuntu@ip-172-31-39-149.ec2.internal>
2019-06-17 17:14:03 +0000

    added limit to make_html and updated index.html

--- make_html.py
+++ make_html.py
@@ -6,11 +6,18 @@ with open('index.html', 'w') as f:
 	f.write('<!DOCTYPE html>')
 	f.write('<html>')
 	f.write('<body>')
+	limit = 5
 	for entry in os.scandir('maps/'):
 		if (entry.is_dir()):
 			f.write('<h1>{}</h1>'.format(entry.name))
-			for mapfile in os.listdir(entry.path):
+			files = os.listdir(entry.path)
+			files.sort()
+			counter = 0
+			for mapfile in files:
+				if (counter >= limit):
+					break
 				print(os.path.join(entry.path, mapfile))
 				f.write('<div class="link"><a href="{}">{}</a></div>'.format(os.path.join(entry.path, mapfile), mapfile.split('_')[1][:-5]))
+				counter += 1
 	f.write('</body>')
 	f.write('</html>')
