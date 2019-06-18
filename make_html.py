b1ffbc8b7854651a9b2933508062fd01653c0e96
Ubuntu <ubuntu@ip-172-31-39-149.ec2.internal>
2019-06-18 17:27:28 +0000

    added first location for each date

--- make_html.py
+++ make_html.py
@@ -1,23 +1,33 @@
 import os
-
+from xml.dom import minidom
 
 
 with open('index.html', 'w') as f:
 	f.write('<!DOCTYPE html>')
 	f.write('<html>')
 	f.write('<body>')
-	limit = 5
+	limit = 25
 	for entry in os.scandir('maps/'):
 		if (entry.is_dir()):
 			f.write('<h1>{}</h1>'.format(entry.name))
 			files = os.listdir(entry.path)
 			files.sort()
 			counter = 0
-			for mapfile in files:
-				if (counter >= limit):
-					break
+			user = entry.name.split('-')[1]
+			for mapfile in files[:limit]:
+				date = mapfile.split('_')[1][:-5]
+				xmldoc = minidom.parse("kml_files/user-{}/history-{}.kml".format(user, date))
+				kml = xmldoc.getElementsByTagName("kml")[0]  # getElementsByTagName always returns NodeList even if there is only one Node so always use [0] to get first Node
+				document = kml.getElementsByTagName("Document")[0]
+				placemarks = document.getElementsByTagName("Placemark")
+				address = ''
+				for placemark in placemarks:
+					address = placemark.getElementsByTagName("address")[0].firstChild
+					if (address != None):	
+						address = address.data
+						print(address)
+						break		
 				print(os.path.join(entry.path, mapfile))
-				f.write('<div class="link"><a href="{}">{}</a></div>'.format(os.path.join(entry.path, mapfile), mapfile.split('_')[1][:-5]))
-				counter += 1
+				f.write('<div class="link"><a href="{}">{}({})</a></div>'.format(os.path.join(entry.path, mapfile), date, address))
 	f.write('</body>')
 	f.write('</html>')
