c67a73416d7252796fa3c70f43dabe0dc2eb6da4
ajikan <az3@illinois.edu>
2019-06-25 15:57:44 -0500

    style buttons

--- helper.js
+++ helper.js
@@ -53,11 +53,11 @@ function download_kml(user, date) {
 function reveal_hidden(id_val) {
     var x = "#";
     var res = x.concat(id_val)
-    $(document).ready(function(){
-        $(this).click(function(){
-            $(res).fadeIn();
-        });
-    });
+    //    $(document).ready(function(){
+    //        $(this).click(function(){
+    $(res).fadeIn();
+    //        });
+    //    });
 }
 
 function toggle(id_val) {
