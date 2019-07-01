0aa26d56636e97dd7daa4aac6bddabe620815b9b
ajikan <az3@illinois.edu>
2019-07-01 11:39:58 -0500

    implement google sign in

--- helper.js
+++ helper.js
@@ -11,18 +11,60 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+//function onSignIn(googleUser) {
+//    var profile = googleUser.getBasicProfile();
+//    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//    console.log('Name: ' + profile.getName());
+//    console.log('Image URL: ' + profile.getImageUrl());
+//    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+//    //window.location = "https://www.google.com/maps/timeline/kml?authuser=0&pb=!1m8!1m3!1i2019!2i5!3i4!2m3!1i2019!2i5!3i4"
+//    //window.location = 'google.com';
+//    console.log('cookie: ' + document.cookie);
+//    // postData(1);
+//   //getData();
+//}
+
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    //window.location = "https://www.google.com/maps/timeline/kml?authuser=0&pb=!1m8!1m3!1i2019!2i5!3i4!2m3!1i2019!2i5!3i4"
-    //window.location = 'google.com';
-    console.log('cookie: ' + document.cookie);
-    // postData(1);
-    //getData();
+  console.log('Google Auth Response', googleUser);
+  // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+  var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+    unsubscribe();
+    // Check if we are already signed-in Firebase with the correct user.
+    if (!isUserEqual(googleUser, firebaseUser)) {
+      // Build Firebase credential with the Google ID token.
+      var credential = firebase.auth.GoogleAuthProvider.credential(
+          googleUser.getAuthResponse().id_token);
+      // Sign in with credential from the Google user.
+      firebase.auth().signInWithCredential(credential).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    } else {
+      console.log('User already signed-in Firebase.');
+    }
+  });
 }
+
+function isUserEqual(googleUser, firebaseUser) {
+  if (firebaseUser) {
+    var providerData = firebaseUser.providerData;
+    for (var i = 0; i < providerData.length; i++) {
+      if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()) {
+        // We don't need to reauth the Firebase connection.
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 
 function postData(input) {
     $.ajax({
--- off_detect.py
+++ off_detect.py
@@ -1,39 +1,69 @@
 import datetime
+import geopy.distance as dist
 import matplotlib.pyplot as plt
 import numpy as np
 from xml.dom import minidom
+import pytz
 
 def plot_bar(labels, times):
-	indexes = np.arange(len(times))
-	plt.bar(indexes, times)
-	plt.xticks(indexes, labels, fontsize=5, rotation=30)
-	plt.xlabel("Dates")
-	plt.ylabel("Duration (minutes)")
-	plt.title("Time Duration Between Google Sends Data")
-	plt.show()
-	
+    indexes = np.arange(len(times))
+    plt.bar(indexes, times, align='center')
+    plt.xticks(indexes, labels, fontsize=5, rotation=30, horizontalalignment='right')
+    plt.xlabel("Dates")
+    plt.ylabel("Duration (minutes)")
+    plt.title("Time Duration Between Google Sends Data")
+    plt.show()
+
+
+def plot_scatter(distances, duration):
+    plt.plot(distances, duration, 'o')
+    plt.xlabel("Distances (miles)")
+    plt.ylabel("Duration (minutes)")
+    plt.title("Time Duration Between Two Coordinates VS Distance Between Two Coordinates")
+    plt.show()
 
 def kml_total():
-	xmldoc = minidom.parse("full_kml_andrew.kml")
-	kml = xmldoc.getElementsByTagName("kml")[0]
-	document = kml.getElementsByTagName("Document")[0]
-	placemark = document.getElementsByTagName("Placemark")[0]
-	track = placemark.getElementsByTagName("gx:Track")[0]
-	times = track.getElementsByTagName("when")
-	index = 0
-	timediffList = [] 
-	dateLabel = []
-	while (index < len(times)-1):
-		timevalue = times[index].firstChild.data[:-1]
-		timevalue_next = times[index+1].firstChild.data[:-1]
-		label = "{}-{}".format(timevalue, timevalue_next)
-		dateLabel.append(label)
-		timevalue = datetime.datetime.strptime(timevalue, "%Y-%m-%dT%H:%M:%S")
-		timevalue_next = datetime.datetime.strptime(timevalue_next, "%Y-%m-%dT%H:%M:%S")
-		timediff = (timevalue_next - timevalue) / datetime.timedelta(minutes=1)
-		timediffList.append(timediff)
-		index += 1
-	plot_bar(dateLabel, timediffList)
+    xmldoc = minidom.parse("full_kml_andrew.kml")
+    kml = xmldoc.getElementsByTagName("kml")[0]
+    document = kml.getElementsByTagName("Document")[0]
+    #placemark = document.getElementsByTagName("Placemark")[0]
+    track = document.getElementsByTagName("gx:Track")[0]
+    times = track.getElementsByTagName("when")
+    coordinates = track.getElementsByTagName("gx:coord")
+    index = 0
+    timediffList = []
+    dateLabel = []
+    distances = []
+    timezone = pytz.timezone("America/Chicago")
+    while (index < len(times)-1):
+        # calculate distance 
+        coord = coordinates[index].firstChild.data[:-1]
+        coord_next = coordinates[index+1].firstChild.data[:-1]
+        coord = coord.split(' ')
+        coord_next = coord_next.split(' ')
+        coord = tuple([float(coord[1]), float(coord[0])])
+        coord_next = tuple([float(coord_next[1]), float(coord_next[0])])
+        distance = dist.distance(coord_next, coord).miles
+        distances.append(distance)
+
+        # calculate time duration
+        timevalue = times[index].firstChild.data[:-1]
+        timevalue_next = times[index+1].firstChild.data[:-1]
+        timevalue = datetime.datetime.strptime(timevalue, "%Y-%m-%dT%H:%M:%S")
+        timevalue_next = datetime.datetime.strptime(timevalue_next, "%Y-%m-%dT%H:%M:%S")
+        timevalue = pytz.utc.localize(timevalue)
+        timevalue_next = pytz.utc.localize(timevalue_next)
+        timevalue = timevalue.astimezone(timezone)
+        timevalue_next = timevalue_next.astimezone(timezone)
+        timediff = (timevalue_next - timevalue) / datetime.timedelta(minutes=1)
+        timediffList.append(timediff)
+        label = "{}-{}".format(timevalue.strftime("%Y-%m-%dT%H:%M:%S"), timevalue_next.strftime("%Y-%m-%dT%H:%M:%S"))
+        dateLabel.append(label)
+        index += 1
+        if (index == 48):
+            break
+    plot_bar(dateLabel, timediffList)
+    plot_scatter(distances, timediffList)
 
 if __name__ == '__main__':
-	kml_total()
+    kml_total()
