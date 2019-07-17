67d26dad1e1ceb3e2c2e3acc77a5448f156b0036
Yuchen Liang <jacksonliang35@gmail.com>
2019-07-17 10:18:05 -0500

    sign out

--- helper.js
+++ helper.js
@@ -202,6 +202,7 @@ function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
+        alert("Signed out successfully!")
         userID = undefined;
         userName = undefined;
         userEmail = undefined;
